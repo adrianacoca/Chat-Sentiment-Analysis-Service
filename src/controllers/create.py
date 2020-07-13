8eb0b15d3340906d225d6661f95bf45ef9f66260
Adriana Coca <coca.adriana@outlook.com>
2020-07-13 18:07:48 +0200

    All create functions working

--- src/controllers/create.py
+++ src/controllers/create.py
@@ -4,7 +4,7 @@ from src.config import *
 from flask import request
 import re
 from flask import Flask
-
+from bson.objectid import ObjectId
 
 client = MongoClient(DBURL)
 db = client.get_database()
@@ -17,24 +17,29 @@ def createUser(username):
     return f"New user created! name: {username} , user id: {userinfo.inserted_id}"
 
 #Chat Endpoints
+
 @app.route ("/chat/create/")
 def createChat():
-    participants = request.args.getlist("participants")
+    participants = [ObjectId(p) for p in request.args.getlist("participants")]
     conv_name = request.args.get("conv_name")
-    conversation = db.chats.insert_one({"conversation name":conv_name, "participants":participants})
+    p_dict={"conversation name":conv_name, "participants":participants}
+    conversation = db.chats.insert_one(p_dict)
     return  f"New chat created! Chat name: {conv_name}, Chat id: {conversation.inserted_id}"
 
-'''
 @app.route ("/chat/<conversation_id>/adduser")
 def user(conversation_id):
-    new_user = request.args.get("user_id")
-    return f"User {user_id} added to chat {conversation_id}!"
+    conversation_id = ObjectId(conversation_id)
+    new_user = ObjectId(request.args.get("user_id"))
+    db.chats.update({ "_id":conversation_id},{ "$push":{ "participants": new_user}})
+    return f"User {new_user} added to chat {conversation_id}!"
 
+#Message Endpoints
 
-@app.route ("/chat/<conversation_id>/addmessage", methods=["POST"])
+@app.route ("/chat/<conversation_id>/addmessage")
 def createConv(conversation_id):
-    conversation = 
-    d={"text":text}
-    message_id=db.conversation_id.insert_one(d)
-    return message_id
-'''
+    conversation_id = ObjectId(conversation_id)
+    participant = ObjectId(request.args.get("user_id"))
+    text = request.args.get("text")
+    conversation = {"chat_id":conversation_id, "user_id":participant, "text":text}
+    db.messages.insert_one(conversation)
+    return f"{text} said by participant: {participant} added to conversation {conversation_id}"
